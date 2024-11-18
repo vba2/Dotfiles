6f9c871f03ae437a6b0d73126b7b07aec73985fd
Josh Strebeck <josh@strebeck.net>
2024-11-17 19:29:04 -0800

    Update deps script

--- deps.sh
+++ deps.sh
@@ -1,21 +1,54 @@
+#!/bin/bash
 #This script will install all deps for the dotfiles in this repo.
+echo "Installing deps for Dotfiles"
 
-stow
-zsh
-say
-ripgrep
-lazygit
-lazydocker
-nvim
-fzf
-linux brew
-copyq
+#Add PPAs
+#sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository ppa:aslatter/ppa -y
+
+sudo apt update && sudo apt upgrade -y
+
+#Install apt
+apt_deps=(stow zsh gnustep-gui-runtime ripgrep neovim copyq sway swaylock waybar rofi alacritty build-essential curl)
+
+for dep in ${apt_deps[@]}
+do
+  sudo apt install $dep -y
+  if [ $? -ne 0 ]; then
+      echo "Could not install $dep"
+      break
+  fi
+done
 
 
-sway
-swaylock
-waybar
-rofi
-mako
-alacritty
+#Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
+brew_deps=(lazygit lazydocker fzf)
+
+for dep in ${brew_deps[@]}
+do
+  sudo brew install $dep -y
+  if [ $? -ne 0 ]; then
+      echo "Could not install $dep"
+      break
+  fi
+done
+
+#Install pip
+brew_pip=(mako)
+
+for dep in ${pip_deps[@]}
+do
+  sudo pip install $dep -y
+  if [ $? -ne 0 ]; then
+      echo "Could not install $dep"
+      break
+  fi
+done
+
+#Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "All deps have been installed"
